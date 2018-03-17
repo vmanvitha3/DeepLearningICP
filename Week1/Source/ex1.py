cef3b7428b5e9dd94c8630761f6d6354d46315a5
vmanvitha3 <vmanvitha3@gmail.com>
2018-03-17 12:03:45 -0500

    Updated Variable Names
    Updated Variable Names

--- Week1/Source/ex1.py
+++ Week1/Source/ex1.py
@@ -3,16 +3,16 @@ import tensorflow as tf
 
 #Create three matrices to perform evaluation of (a^2+b)*c
 #Creates 2x2 matrices
-Matrix_one = tf.constant([[2,3],[3,4]])
-Matrix_two = tf.constant([[2,3],[3,4]])
-Matrix_three = tf.constant([[5,7],[1,3]])
+a = tf.constant([[2,3],[3,4]])
+b = tf.constant([[2,3],[3,4]])
+c = tf.constant([[5,7],[1,3]])
 
-#we use square method to square Matrix_one and result is added to Matrix_two and stored in the variable add
+#we use square method to square a and result is added to b and stored in the variable add
 
-add = tf.add(tf.square(Matrix_one),Matrix_two)
+add = tf.add(tf.square(a),b)
 
-#We then multiply varibale add with Matrix_three to get final result which is stored in final variable
-final = tf.matmul(add,Matrix_three)
+#We then multiply varibale add with c to get final result which is stored in final variable
+final = tf.matmul(add,c)
 #b = tf.pow(Matrix_one,2)
 
 #we create a session object which will let us execute and evaluate object and finally close the session once our work is completed
